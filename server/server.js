f12654c48e06b874d150dc63aa6428e5fcda3f89
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-03-17 11:13:10 +0530

    Backend is live on localhost

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config()
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send("API WORKING!")
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+  console.log(`Server Running At Port http://localhost:${port}`)
+})
