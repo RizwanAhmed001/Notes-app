2113df241504c12e161b9d0ba0df81d7d054d16a
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-03-17 12:06:43 +0530

    Connect to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/connectDB.js";
 dotenv.config()
 
 const app = express();
+connectDB();
 
 app.use(express.json());
-app.use(cors())
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 app.get("/", (req, res) => {
   res.send("API WORKING!")
