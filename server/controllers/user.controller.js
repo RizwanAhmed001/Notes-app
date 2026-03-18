dbd3e6997c8782a30322c893160451d9de629f83
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-03-18 18:35:52 +0530

    Login api is added

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -22,7 +22,9 @@ export const register = async (req, res) => {
 
     await newUser.save();
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
     res.cookie("token", token, {
       httpOnly: true,
@@ -31,9 +33,48 @@ export const register = async (req, res) => {
       secure: false,
     });
 
-    const userData = {name: newUser.name}
+    const userData = { name: newUser.name };
 
-    return res.json({success: true, message: "User Registered", userData});
+    return res.json({ success: true, message: "User Registered", userData });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.json({ success: false, message: "All Fields Are Mandatory!" });
+    }
+
+    const emailExist = await UserModel.findOne({ email });
+
+    if (!emailExist) {
+      return res.json({ success: false, message: "Invalid Credentials" });
+    }
+
+    const matchPassword = await bcrypt.compare(password, emailExist.password);
+
+    if (!matchPassword) {
+      return res.json({ success: false, message: "Invalid Credentials" });
+    }
+
+    const token = jwt.sign({ id: emailExist._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+      sameSite: "lax",
+      secure: false
+    })
+
+    const userData = { name: emailExist.name };
+
+    return res.json({success: true, message: "User Login", userData})
 
   } catch (error) {
     return res.json({ success: false, message: error.message });
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/user.controller.js";
+import { login, register } from "../controllers/user.controller.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/register", register);
+userRoute.post("/login", login);
 
 export default userRoute;
