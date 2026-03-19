a7dfd92781add85d9fe47b7f08ddc66f2ff8b819
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-03-19 17:32:38 +0530

    Update note pin and update note api is added

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -35,11 +35,85 @@ export const addNote = async (req, res) => {
 
 export const getAllNotes = async (req, res) => {
   try {
+    const allNotes = await NoteModel.find({ user: req.userId });
 
-    const allNotes = await NoteModel.find({user: req.userId})
+    return res.json({ success: true, message: "All Notes", allNotes });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
 
+export const updatePin = async (req, res) => {
+  try {
 
-    return res.json({success: true, message: "All Notes", allNotes});
+    const { noteid } = req.params;
+
+    const userId = req.userId;
+
+    if (!noteid) {
+      return res.json({ success: false, message: "Note Id is Required!" });
+    }
+
+    const note = await NoteModel.findById(noteid);
+
+    if (!note) {
+      return res.json({
+        success: false,
+        message: "No Note found",
+      });
+    }
+
+    if (note.user.toString() !== userId) {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+
+    note.pin = !note.pin;
+
+    await note.save();
+
+    return res.json({ success: true, message: "Note Updated", note });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export const updateNote = async (req, res) => {
+  try {
+
+    const {noteid} = req.params;
+
+    const userId = req.userId
+
+    if(!noteid){
+      return res.json({success: false, message: "Note Id Is Required!"})
+    }
+
+    const {title, content, tags} = req.body;
+
+    if(!title || !content || !title.trim() || !content.trim()){
+      return res.json({success:false, message: "Title & Content is mnadatory!"})
+    }
+
+     if (tags && !Array.isArray(tags)) {
+      return res.json({
+        success: false,
+        message: "Tags must be an array",
+      });
+    }
+
+    const note = await NoteModel.findById(noteid);
+
+    if(note.user.toString() !== userId){
+      return res.json({success : false, message: "Unauthorized"})
+    }
+
+    const updateNote = await NoteModel.findByIdAndUpdate({_id: noteid}, {title, content, tags}, {new: true});
+
+    if(!updateNote){
+      return res.json({success: false, message: "Note Not Found!"})
+    }
+
+    return res.json({success: true, message: "Note Updated", updateNote})
 
   } catch (error) {
     return res.json({ success: false, message: error.message });
--- server/routes/note.route.js
+++ server/routes/note.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { addNote, getAllNotes } from "../controllers/note.controller.js";
+import { addNote, getAllNotes, updateNote, updatePin } from "../controllers/note.controller.js";
 import { auth } from "../middleware/auth.js";
 
 const noteRoute = express.Router();
 
 noteRoute.post("/addnote", auth, addNote);
 noteRoute.get("/getnotes", auth, getAllNotes);
+noteRoute.put("/updatepin/:noteid", auth, updatePin);
+noteRoute.put("/updatenote/:noteid", auth, updateNote);
 
 export default noteRoute;
