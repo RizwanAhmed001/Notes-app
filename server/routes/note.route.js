61b1f5cd2bf08f08087e026f81b8b7e8900b3e64
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-03-19 18:08:54 +0530

    Backend is Done

--- server/routes/note.route.js
+++ server/routes/note.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addNote, getAllNotes, updateNote, updatePin } from "../controllers/note.controller.js";
+import { addNote, deleteNote, getAllNotes, updateNote, updatePin } from "../controllers/note.controller.js";
 import { auth } from "../middleware/auth.js";
 
 const noteRoute = express.Router();
@@ -8,5 +8,6 @@ noteRoute.post("/addnote", auth, addNote);
 noteRoute.get("/getnotes", auth, getAllNotes);
 noteRoute.put("/updatepin/:noteid", auth, updatePin);
 noteRoute.put("/updatenote/:noteid", auth, updateNote);
+noteRoute.delete("/deletenote/:noteid", auth, deleteNote);
 
 export default noteRoute;
