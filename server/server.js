9d26d1e27d789c24a38221182db1a9bf64d7e723
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-03-26 18:15:42 +0530

    All Done

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+    "https://notes-app-frontend-td95.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
