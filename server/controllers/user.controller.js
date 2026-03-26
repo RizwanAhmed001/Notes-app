948f3b2d4616d0a08782982691e8200b7f10ee33
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-03-26 18:33:25 +0530

    change samsite to none from lax

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -29,7 +29,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
     });
 
@@ -68,7 +68,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
     });
 
@@ -84,7 +84,7 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
     });
 
